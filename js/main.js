6a23b7e4e6e044a7e7c2a28fe6b47d3da655ef6b
Aishwary Gupta <aish042004@gmail.com>
2025-04-11 23:38:59 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var gl = GL.create();
+var water;
+var cubemap;
+var renderer;
+var angleX = -25;
+var angleY = -200.5;
+
+// Sphere physics info
+var useSpherePhysics = false;
+var center;
+var oldCenter;
+var velocity;
+var gravity;
+var radius;
+var paused = false;
+
+window.onload = function () {
+  // ...existing onload logic...
+};
